fd0968ea81ac80cf2ed3d7eeb7e8b9c124718340
abheek-rajeev <abheekrajeev@gmail.com>
2023-11-21 03:44:33 +0530

    middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,7 @@
 import bcryptjs from "bcrypt";
 import User from "../user.model.js";
 
-export const signup=async(req,res)=>{
+export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
     const hashedPassword=bcryptjs.hashSync(password,10);
     const newUser = new User({username,email,password: hashedPassword});
@@ -11,7 +11,7 @@ export const signup=async(req,res)=>{
         res.status(201).json('user created succfully')
     }
     catch(err){
-        res.status(500).json(err.message)
+        next(err);
     }
 
 
