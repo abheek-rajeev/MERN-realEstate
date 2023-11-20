76d29aa2565737bd16b7dfdd48f897896e445eab
abheek-rajeev <abheekrajeev@gmail.com>
2023-11-21 03:15:22 +0530

    create signup and route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+
+import bcryptjs from "bcrypt";
+import User from "../user.model.js";
+
+export const signup=async(req,res)=>{
+    const {username,email,password}=req.body;
+    const hashedPassword=bcryptjs.hashSync(password,10);
+    const newUser = new User({username,email,password: hashedPassword});
+    try{
+        await newUser.save();
+        res.status(201).json('user created succfully')
+    }
+    catch(err){
+        res.status(500).json(err.message)
+    }
+
+
+
+}
---
+++ api/routes/auth.route.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { signup } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+export default router.post("/signup",signup)
